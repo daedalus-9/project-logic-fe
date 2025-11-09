3048fe5b969e020715b9ad8baa76ceff089b1e20
Adam Young <adamyoungy678@gmail.com>
2025-11-09 23:32:29 +0000

    fix: buttons

--- src/components/features/grid/FeatureGrid.tsx
+++ src/components/features/grid/FeatureGrid.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Content } from "./Content";
 import { CornerBlur } from "@/components/utils/CornerBlur";
 import { CornerGrid } from "@/components/utils/CornerGrid";
+import { SplashButton } from "@/components/buttons/SplashButton";
+import { scrollToForm } from "@/components/utils/scrollToForm";
+import { FiArrowRight } from "react-icons/fi";
 
 export const FeatureGrid = () => {
   return (
@@ -9,6 +12,13 @@ export const FeatureGrid = () => {
       <Content />
       <CornerBlur />
       <CornerGrid />
+      <SplashButton
+        onClick={() => scrollToForm()}
+        className="mx-auto mt-10 flex items-center gap-2"
+      >
+        Get started
+        <FiArrowRight />
+      </SplashButton>
     </div>
   );
 };
